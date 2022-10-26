5b23323c20544237fc0e9177a45f310ec5c39e86
psalmseen <samsonoyebamiji02@gmail.com>
2022-10-26 08:08:20 +0100

    added the fix

--- functions/server.ts
+++ functions/server.ts
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ credentials: true }));
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
 
 app.use(router);
 app.use('/auth', authRouter);
