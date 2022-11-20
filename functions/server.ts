8bfdaf0742857c758a5d5214e0f59af5efe825e3
psalmseen <samsonoyebamiji02@gmail.com>
2022-11-21 00:27:07 +0100

    checks the access token

--- functions/server.ts
+++ functions/server.ts
@@ -14,6 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+app.set('trust proxy', 1);
 
 app.use(router);
 app.use('/auth', authRouter);
